fbeeddf08d8a4f896eb35818c1c18c7a35ea35b4
Harsha Doddipatla <harshadoddipatla@Harshas-MacBook-Air.local>
2023-12-15 08:24:53 +0530

    fixed the eventhandler

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home.js";
+import MovieDetails from "./components/MovieDetails/MovieDetails";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import Footer from "./components/Footer/Footer";
+import "./App.scss";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+        <Header></Header>
+        <div className="container">
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/movie/:imdbid" element={<MovieDetails />} />
+          <Route element={<PageNotFound />} />
+        </Routes>
+        </div>
+        <Footer/>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
